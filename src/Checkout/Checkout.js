968111d8af7f5d42e6714ae6d254fdbd68e2344f
Oliver Oxenham <oliver@cardcity.co>
2019-06-04 15:21:43 +0400

    update checkout

--- src/Checkout/Checkout.js
+++ src/Checkout/Checkout.js
@@ -3,16 +3,12 @@ import CheckoutForm from './CheckoutForm';
 import { StripeProvider, Elements } from 'react-stripe-elements';
 
 const Checkout = () => {
-  console.log('context=', process.env, process.env.CONTEXT);
-  const STRIPE_PUBLISHABLE_KEY =
-    process.env.NODE_ENV === 'production'
-      ? process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY
-      : process.env.REACT_APP_DEV_STRIPE_PUBLISHABLE_KEY;
-  console.log(process.env);
-  console.log('env=', process.env.NODE_ENV);
-  console.log('STRIPE_PUBLISHABLE_KEY=', STRIPE_PUBLISHABLE_KEY);
+  console.log(
+    'STRIPE_PUBLISHABLE_KEY=',
+    process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY
+  );
   return (
-    <StripeProvider apiKey={`${STRIPE_PUBLISHABLE_KEY}`}>
+    <StripeProvider apiKey={`${process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY}`}>
       <Elements>
         <CheckoutForm />
       </Elements>
